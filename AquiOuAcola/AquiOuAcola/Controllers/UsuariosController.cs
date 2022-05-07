2e940eb429f80bf3395da20e24e951548c814fc3
GOliveira15 <gabrielosilva15@gmail.com>
2022-05-07 18:37:11 -0300

    Primeira página

---
+++ AquiOuAcola/AquiOuAcola/Controllers/UsuariosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AquiOuAcola.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult cadastrarUsuario()
+        {
+            return View();
+        }
+    }
+}
